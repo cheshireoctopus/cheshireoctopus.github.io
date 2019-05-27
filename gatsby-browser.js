4bdbae7a6469a9748f73ac758f84ffb9bc892895
cheshireoctopus <chandlermoisen@gmail.com>
2019-05-27 18:12:42 -0400

    Tracking and syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,3 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import 'prismjs/themes/prism-tomorrow.css'
+import 'typeface-montserrat'
+import 'typeface-merriweather'
