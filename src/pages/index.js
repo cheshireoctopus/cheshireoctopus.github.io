7290cc8d6e9e2eb3e439317a8b492e6ae306bd48
cheshireoctopus <chandlermoisen@gmail.com>
2019-05-30 17:39:53 -0400

    minor refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,36 +6,39 @@ import SEO from '../components/seo'
 import { rhythm } from '../utils/typography'
 
 class BlogIndex extends React.Component {
-  render() {
+  renderPosts() {
     const { data } = this.props
+    const { posts } = data.allMarkdownRemark.edges
+
+    return posts.map(({ node }) => {
+      const title = node.frontmatter.title || node.fields.slug
+
+      return (
+        <div key={node.fields.slug}>
+          <h3 style={{ marginBottom: rhythm(1 / 4) }}>
+            <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+              {title}
+            </Link>
+          </h3>
+          <small>{node.frontmatter.date}</small>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: node.frontmatter.description || node.excerpt,
+            }}
+          />
+        </div>
+      )
+    })
+  }
+
+  render() {
+    const { data, location } = this.props
     const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={location} title={siteTitle}>
         <SEO title="All posts" />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          )
-        })}
+        {this.renderPosts()}
       </Layout>
     )
   }
