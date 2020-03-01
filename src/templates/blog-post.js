fbf22ee64f8a5133a72e16c543970f68115aba83
cheshireoctopus <chandlermoisen@gmail.com>
2020-02-29 20:24:46 -0500

    Adds blog heading which renders on window scroll

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
+
+const ScrolledHeading = styled.div`
+  background: white;
+  box-shadow: 1px 1px 3px black;
+  display: ${({ render }) => render ? 'flex' : 'none'};
+  left: 0;
+  padding: 16px;
+  position: fixed;
+  opacity: ${({ render }) => render ? 1 : 0};
+  right: 0;
+  top: 0;
+  z-index: 1;
+`
+
+const ScrolledTitle = styled.div`
+  font-family: 'Roboto Slab',sans-serif;
+  font-size: 1em;
+`;
+
+const ScrolledHomeLink = styled(Link)`
+  background-image: none;
+  margin-right: 16px;
+`;
 
 const BlogPostTemplate = ({
   data,
@@ -13,6 +37,19 @@ const BlogPostTemplate = ({
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata.title
   const { previous, next } = pageContext
+  const [scrolledHeader, setScrolledHeader] = useState(false)
+
+  useEffect(() => {
+    const scrollEvent = window.addEventListener('scroll', () => {
+      if (window.scrollY > 150) {
+        setScrolledHeader(true)
+      } else {
+        setScrolledHeader(false)
+      }
+    })
+
+    return () => window.removeEventListener('scroll', scrollEvent)
+  }, [])
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -21,10 +58,16 @@ const BlogPostTemplate = ({
         description={post.frontmatter.description || post.excerpt}
       />
       <h1>{post.frontmatter.title}</h1>
+      <ScrolledHeading render={scrolledHeader}>
+        <ScrolledHomeLink to="/">
+          ←
+        </ScrolledHomeLink>
+        <ScrolledTitle>{post.frontmatter.title}</ScrolledTitle>
+      </ScrolledHeading>
       <p
         style={{
           ...scale(-1 / 5),
-          display: `block`,
+          display: 'block',
           marginBottom: rhythm(1),
           marginTop: rhythm(-1),
         }}
