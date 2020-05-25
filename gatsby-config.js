6facb62f1f5b503565636e35e392205a67f170fb
cheshireoctopus <chandlermoisen@gmail.com>
2020-05-25 13:27:58 -0400

    Adds CodePen markdown processor; adds "Box Shdow Border Radius" post

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,13 @@ module.exports = {
               wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
+          {
+            resolve: '@weknow/gatsby-remark-codepen',
+            options: {
+              theme: 'dark',
+              height: 400,
+            },
+          },
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
