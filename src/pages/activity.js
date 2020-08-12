cf794921a45076ea7d33ca00d8a1d757f8ba7aa3
cheshireoctopus <chandlermoisen@gmail.com>
2020-08-11 21:14:13 -0600

    Fixes import

--- src/pages/activity.js
+++ src/pages/activity.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import { PageHeading } from '../components/styled-components'
@@ -12,14 +13,13 @@ const Activity = ({ location }) => {
       <PageHeading>Activity</PageHeading>
 
       <p>
-        I use <a href="https://wakatime/com">WakaTime</a> to track time spent developing.
-        Chances are the following chart is composed largely of JavaScript.
+        I use <a href="https://wakatime/com">WakaTime</a> to track time spent
+        developing. Chances are the following chart is composed largely of
+        JavaScript.
       </p>
 
       <figure>
-        <embed
-          src="https://wakatime.com/share/@8ac2ead5-5dea-4c58-8987-d14005828cc0/932f6686-70c2-4986-a94b-df89f9148e51.svg"
-        />
+        <embed src="https://wakatime.com/share/@8ac2ead5-5dea-4c58-8987-d14005828cc0/932f6686-70c2-4986-a94b-df89f9148e51.svg" />
       </figure>
     </Layout>
   )
