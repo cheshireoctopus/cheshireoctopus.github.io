561f279de2abbbc77a7c52425e29db6dd2700cbb
cheshireoctopus <chandlermoisen@gmail.com>
2020-09-05 15:04:45 -0600

    Adds BlogPostDirectionalNavigation component; adds Appear to blog-post

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,15 @@
 export { default as Appear } from './Appear'
 export { default as Bio } from './Bio'
 export { default as BlogPost } from './BlogPost'
+export {
+  default as BlogPostDirectionalNavigation,
+} from './BlogPostDirectionalNavigation'
 export { default as Footer } from './Footer'
 export { default as Header } from './Header'
 export { default as Layout } from './layout'
-export { PageHeading } from './styled-components'
 export { default as Pagination } from './Pagination'
 export { default as SEO } from './seo'
 export { default as TagsRotator } from './TagsRotator'
 export { default as Theme } from './Theme'
+
+export { BlogPostDate, PageHeading } from './styled-components'
--- src/components/styled-components.js
+++ src/components/styled-components.js
@@ -9,3 +9,15 @@ export const PageHeading = styled.h1`
     font-size: ${({ theme }) => theme.fontSizes[10]}px;
   }
 `
+
+export const BlogPostDate = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes[3]}px;
+  font-style: italic;
+  margin-bottom: ${({ theme }) => theme.space[5]}px;
+  text-align: center;
+
+  ${({ theme }) => theme.mediaQueries.medium} {
+    font-size: ${({ theme }) => theme.fontSizes[4]}px;
+    margin-bottom: ${({ theme }) => theme.space[7]}px;
+  }
+`
